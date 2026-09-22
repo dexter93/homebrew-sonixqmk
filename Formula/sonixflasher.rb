35cf7810c57568b165caa9126e46601d8d40185e
Dimitris Mantzouranis <d3xter93@gmail.com>
2026-09-22 13:28:28 +0300

    bump-sonixflasher-3.0.0

--- Formula/sonixflasher.rb
+++ Formula/sonixflasher.rb
@@ -1,24 +1,12 @@
 class Sonixflasher < Formula
-  desc "CLI-based Flasher for Sonix SN32F2xx MCUs"
+  desc "CLI-based flasher and diagnostics tool for Sonix SN32F2xx USB bootloader devices"
   homepage "https://github.com/SonixQMK/SonixFlasherC"
-  url "https://github.com/SonixQMK/SonixFlasherC/archive/refs/tags/2.0.8.tar.gz"
-  sha256 "fb84fca699d7a91e2bfcfe6aad00a5967f6ad08606c602d49d37d5a7a7fa0db8"
+  url "https://github.com/SonixQMK/SonixFlasherC/archive/refs/tags/3.0.0.tar.gz"
+  sha256 "2b8f377544f91e857e10dea35cbadd26062f81a17a71011145f1214b68428044"
   license "GPL-3.0-only"
 
-  bottle do
-    root_url "https://ghcr.io/v2/sonixqmk/sonixqmk"
-    sha256 cellar: :any, arm64_sequoia: "3061076268cd7c2194515afb6fb695eabd4215be3345a71b53cabfdaf4bdb9ed"
-    sha256 cellar: :any, arm64_sonoma:  "5664effea348c5252f774dee19343a6cd39094d6389f7175e505099f81f62339"
-    sha256 cellar: :any, ventura:       "fec301d557b2ec03b7c79a716977d91478d8dafa173b3365ad38e3e85e5d4b0e"
-  end
-
   depends_on "pkg-config" => :build
-  depends_on "hidapi"
-
-  on_linux do
-    depends_on "libusb"
-    depends_on "systemd" # for libudev
-  end
+  depends_on "libusb"
 
   def install
     system "make", "clean", "sonixflasher"
@@ -26,7 +14,7 @@ class Sonixflasher < Formula
   end
 
   test do
-    output = shell_output("#{bin}/sonixflasher -V 2>&1", 1)
-    assert_match(/sonixflasher \d+\.\d+\.\d+\n/, output)
+    output = shell_output("#{bin}/sonixflasher -V")
+    assert_match(/sonixflasher \d+\.\d+\.\d+/, output)
   end
 end
